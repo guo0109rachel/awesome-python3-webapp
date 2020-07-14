f9363e57d5a72650284231c595d1ac2ecded32e1
guo0109rachel <guomq920418@gmail.com>
2020-07-14 13:08:47 -0400

    After day9 Implementation

--- www/models.py
+++ www/models.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'Mengqing Guo'
+
 import time, uuid
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
